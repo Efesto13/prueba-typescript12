ae1886263d8124912884680944211b669bc6e926
Efesto13 <ruedasamuel999@gmail.com>
2026-05-15 08:54:58 -0500

    new comment

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { validateAccessToken } from './lib/jwt';
-
+//proxy
 const PUBLIC_PAGE = ['/login', '/register'];
 const PROTECTED_PAGE = [
     '/masterAdmin',
